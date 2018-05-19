4426ad10588a0b9ec9d2cc59944a7ae40d7072bb
ngocvantu <tunguyen4078@gmail.com>
2018-05-19 21:52:30 +0700

    asdfasdf

---
+++ src/GoWeb1/client2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os/exec"
+	"log"
+	"fmt"
+)
+
+func main(){
+	//con,_ := net.Dial("tcp", "45.32.118.97:8070")
+	//defer con.Close()
+	//io.Copy(os.Stdout, con)
+	//res, _ := http.Get("http://dantri.com.vn/")
+	//defer res.Body.Close()
+	//
+	//doc, _ := html.Parse(res.Body)
+	//fmt.Println(string(doc.))
+
+	out, err := exec.Command("yum clean all").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", out)
+}
